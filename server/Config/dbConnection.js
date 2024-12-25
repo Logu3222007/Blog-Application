3be62eb351d5899833c0147e8516325c5c094dd6
Logu3222007 <logu2007dev@gmail.com>
2024-12-25 18:18:22 +0530

    Update dbConnection.js

--- server/Config/dbConnection.js
+++ server/Config/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/Blog-App')
+        await mongoose.connect('mongodb+srv://logu2007dev:Logu%402007@cluster0.bga3r.mongodb.net/Blog-Application')
             .then(() => { console.log("Mongodb is Connected!") })
     } catch (err) {
         console.error('MongoDB connection error:', err.message);
