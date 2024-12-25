39c03755b91ba631ee412571787bf46dc96a34d1
Logu3222007 <logu2007dev@gmail.com>
2024-12-25 19:01:16 +0530

    Update ViewAllPosts.js

--- client/src/Regular/ViewAllPosts.js
+++ client/src/Regular/ViewAllPosts.js
@@ -17,7 +17,7 @@ function ViewAllPosts() {
       });
       setViewAllPosts(res.data.GetViewAllPosts);
     } catch (err) {
-      toast.error("Failed to fetch posts. Please try again.");
+      console.log("Failed to fetch posts. Please try again.");
     }
   };
   useEffect(()=>{
