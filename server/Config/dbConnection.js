9ddf742c0452e32c63aa8182792f46e9ceacd43d
Logu3222007 <logu2007dev@gmail.com>
2024-12-25 18:27:50 +0530

    Update dbConnection.js

--- server/Config/dbConnection.js
+++ server/Config/dbConnection.js
@@ -2,11 +2,17 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://logu2007dev:Logu%402007@cluster0.bga3r.mongodb.net/Blog-Application')
-            .then(() => { console.log("Mongodb is Connected!") })
+        await mongoose.connect('mongodb+srv://logu2007dev:Logu%402007@cluster0.bga3r.mongodb.net/Blog-Application', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000,  // Timeout for server selection
+            socketTimeoutMS: 45000,         // Socket timeout (higher for slow networks)
+        });
+        console.log("MongoDB is Connected!");
     } catch (err) {
         console.error('MongoDB connection error:', err.message);
         process.exit(1); // Exit the process if the connection fails
     }
 };
-module.exports = { connectDB }
+
+module.exports = { connectDB };
