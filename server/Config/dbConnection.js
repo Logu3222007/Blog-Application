8cf910e551629371f3bd4b7cb2dcfce06b211b9a
Logu3222007 <logu2007dev@gmail.com>
2024-12-25 18:40:54 +0530

    Update dbConnection.js

--- server/Config/dbConnection.js
+++ server/Config/dbConnection.js
@@ -6,9 +6,7 @@ const connectDB = async () => {
         await mongoose.connect('mongodb+srv://logu2007dev:Logu%402007@cluster0.bga3r.mongodb.net/Blog-Application', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            connectTimeoutMS: 30000, // Increase timeout to 30 seconds
-            useFindAndModify: false, // Prevent deprecated warnings
-            useCreateIndex: true, // Prevent deprecated warnings
+            connectTimeoutMS: 30000, // Optional: Increase timeout to 30 seconds
         });
         console.log("MongoDB connected successfully!");
     } catch (err) {
